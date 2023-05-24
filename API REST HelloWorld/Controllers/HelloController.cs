3a9c0ae3dba3ddcae317902da238beb298ed1407
s2y-404 <75375209+s2y-404@users.noreply.github.com>
2023-05-24 22:49:23 +0200

    Update HelloController.cs

--- API REST HelloWorld/Controllers/HelloController.cs
+++ API REST HelloWorld/Controllers/HelloController.cs
@@ -8,31 +8,21 @@ namespace HelloWorld.Controllers
     [ApiController]
     public class HelloController : ControllerBase
     {
-        private readonly ILogger<HelloController> _logger;
-
-        /// <summary></summary>
-        public HelloController(ILogger<HelloController> logger)
-        {
-            _logger = logger;
-        }
-
         /// <summary> 
-        /// Méthode salutation d'un utilisateur 
+        /// MÃ©thode salutation d'un utilisateur 
         /// </summary> 
         /// <param name="firstname">Instance <see cref="string"/> avec le nom de l'utilisateur</param> 
         /// <returns>Instance <see cref="Object"/> "Hello <paramref name="firstname"/>!"; sinon, message d'erreur</returns> 
-        /// <exception cref="System.ArgumentException">
-        /// <paramref name="firstname"/> est <c>null</c> ou vide.
-        /// </exception>
+        /// <response code="200">Ok<response>
+        /// <response code="400">Bad Request<response>
         [HttpGet("HelloWorld")]
         public IActionResult HelloWorld(string ?firstname)
         {
             if (string.IsNullOrWhiteSpace(firstname))
             {
-                string error = "Aucun nom n'a été renseigné";
+                string error = "Aucun nom n'a Ã©tÃ© renseignÃ©";
                 //le message retour est converti en Object JSON
                 return BadRequest(new { error });
-                //throw new ArgumentException(message: "Aucun nom n'a été renseigné");
             }
 
             string message = $"Hello {firstname}!";
